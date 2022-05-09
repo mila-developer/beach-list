fab4c4eaee5c98617aef189f9ca407afafad5ce8
Camila Abreu <camila.abreu@grupofullsix.pt>
2022-05-09 22:59:21 +0100

    ADD: beachList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, NativeBaseProvider, Text } from "native-base";
+import React, {useContext, useEffect, useState} from 'react';
+import { HStack, VStack, Center, Checkbox, Input, Button } from "native-base";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const LinearGradient = require("expo-linear-gradient").LinearGradient;
+const config = {
+  dependencies: {
+    "linear-gradient": LinearGradient
+  }
+};
+const beachItems = [
+  "Protetor solar",
+  "Carteira",
+  "Toalha",
+  "Água",
+  "Óculos escuro",
+  "Fones de ouvido",
+];  
+
+const App = (props) => {
+  const [inputText, setInputText] = useState('');
+  const [displayItems, setDisplayItems] = useState([]);
+  useEffect(() => {
+
+     
+
+  }, [displayItems]);
+  const handleClick = () => {
+    console.log("inputText = " +inputText);
+    displayItems.push(inputText);
+    console.log("displayItems = " +displayItems);
+    setDisplayItems(displayItems);
+  }
+  const handleChange = text => setInputText(text);
+
+  return <NativeBaseProvider config={config}>
+  <Center>
+    <Text fontSize="6xl">Beach List</Text>
+    <Input w="75%" maxW="300px" mb="3" py="0" onChangeText={handleChange}
+    InputRightElement={
+      <Button size="xs" rounded="none" w="1/6" h="full" onPress={handleClick}>
+    Ok
+      </Button>} placeholder="Digite o item" />
+    <VStack space={4}>
+    {
+      displayItems.map(item => {
+        return (
+        <HStack space={3}>
+          <Checkbox value="test">
+          </Checkbox>
+          <Text color="black.100">{item}</Text>
+        </HStack>
+      );
+      })
+    }
+    </VStack>
+  </Center>
+  </NativeBaseProvider>;
+};
 
 export default App;
