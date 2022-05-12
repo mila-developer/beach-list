da208da5ab547f98d29be6c50f601372f6be3453
Camila Abreu <camila.abreu@grupofullsix.pt>
2022-05-12 10:16:43 +0100

    UPD: bg linear gradient

--- src/App.js
+++ src/App.js
@@ -29,28 +29,36 @@ const App = (props) => {
   const handleChange = text => setInputText(text);
 
   return <NativeBaseProvider config={config}>
-  <Center>
-    <Text fontSize="6xl">Beach List</Text>
-    <Input w="75%" maxW="300px" mb="3" py="0" onChangeText={handleChange}
-    InputRightElement={
-      <Button size="xs" rounded="none" w="1/6" h="full" onPress={handleClick}>
-    Ok
-      </Button>} placeholder="Digite o item" />
-    <VStack space={4}>
-    {
+  <Box w="100vw" h="100vh" bg={{
+    linearGradient: {
+      colors: ["lightBlue.300", "violet.800"],
+      start: [0, 0],
+      end: [1, 0]
+    }  
+  }}>
+    <Center>
+      <Text fontSize="6xl" color="white">Beach List</Text>
+      <Input w="75%" maxW="300px" mb="3" py="0" onChangeText={handleChange}
+      InputRightElement={
+        <Button size="xs" rounded="none" w="1/6" h="full" onPress={handleClick}>
+      Ok
+        </Button>} color="white" placeholder="Insert your item" />
+      <VStack space={4}>
+      {
 
-      displayItems.map(item => {
-        return (
-        <HStack space={3}>
-          <Checkbox value="test">
-          </Checkbox>
-          <Text color="black.100">{item}</Text>
-        </HStack>
-      );
-      })
-    }
-    </VStack>
-  </Center>
+        displayItems.map(item => {
+          return (
+          <HStack space={3}>
+            <Checkbox value="test">
+            </Checkbox>
+            <Text color="white">{item}</Text>
+          </HStack>
+        );
+        })
+      }
+      </VStack>
+    </Center>
+  </Box>
   </NativeBaseProvider>;
 };
 
